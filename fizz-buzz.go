223236e77328a5d1bc534622801e6d278aed8e46
him <im@him.is>
2022-07-30 04:49:36 +0100

    add/update Golang solutions

--- fizz-buzz.go
+++ fizz-buzz.go
@@ -1,5 +1,5 @@
-// 193 chars
+// 186 chars
 
 package main
 import x"fmt"
-func main(){for i:=1; i<101; i++{if i%3==0&&i%5==0 {x.Println("FizzBuzz")} else if i%3==0 {x.Println("Fizz")} else if i%5==0 {x.Println("Buzz")} else {x.Println(i)}}}
+func main(){for i:=1; i<101; i++{if i%3==0&&i%5==0{x.Println("FizzBuzz")}else if i%3==0{x.Println("Fizz")}else if i%5==0{x.Println("Buzz")}else{x.Println(i)}}}
