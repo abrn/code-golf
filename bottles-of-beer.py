a1f2df8a82587d7420e7ec6f52f8f4672848b9ad
him <im@him.is>
2022-07-30 03:44:48 +0100

    [bottles-of-beer] remove 8 characters

--- bottles-of-beer.py
+++ bottles-of-beer.py
@@ -1,7 +1,14 @@
-# 331 chars
+# 323 chars
 
-b,w='bottles of beer','on the wall'
-m,t,l,k='more','Take one down and pass it around, ','1 bottle of beer',f'{b} {w}'
-for i in range(99,2,-1):
-    print(f'{i} {k}, {i} {b}.\n{t}{i-1} {k}.\n')
-print(f'2 {k}, 2 {b}.\n{t}{l} {w}.\n\n{l} {w}, {l}.\n{t}no {m} {k}.\n\nNo {m} {k}, no {m} {b}.\nGo to the store and buy some {m}, 99 {k}.')
+H=print
+B,D='bottles of beer','on the wall'
+C,E,F,A='more','Take one down and pass it around, ','1 bottle of beer',f"{B} {D}"
+for G in range(99,2,-1):H(f"{G} {A}, {G} {B}.\n{E}{G-1} {A}.\n")
+H(f"""2 {A}, 2 {B}.
+{E}{F} {D}.
+
+{F} {D}, {F}.
+{E}no {C} {A}.
+
+No {C} {A}, no {C} {B}.
+Go to the store and buy some {C}, 99 {A}.""")
